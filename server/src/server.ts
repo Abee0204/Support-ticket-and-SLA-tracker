3bf98dbfa7ca5b52588f7358a3e1a32686c7f28a
Abee0204 <abhayrautela6811@gmail.com>
2026-08-29 16:04:50 +0530

    server cors fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,64 @@
 import { createServer } from "node:http";
+import { createYoga, createSchema } from "graphql-yoga";
+import { readFileSync } from "fs";
+import dotenv from "dotenv";
+import { verifyToken } from "./modules/utils/jwt";
 
-const port = Number(process.env.PORT) || 4000;
+import { authResolvers } from "./modules/auth/auth.resolver";
+import { ticketResolvers } from "./modules/ticket/ticket.resolver";
 
-const server = createServer((req, res) => {
-  console.log("Incoming:", req.url);
+dotenv.config();
 
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("OK WORKING 🚀");
+const typeDefs = readFileSync("./src/schema.graphql", "utf-8");
+
+const resolvers = {
+  Query: {
+    ...authResolvers.Query,
+    ...ticketResolvers.Query,
+  },
+  Mutation: {
+    ...authResolvers.Mutation,
+    ...ticketResolvers.Mutation,
+  },
+  Ticket: ticketResolvers.Ticket,
+};
+
+const schema = createSchema({
+  typeDefs,
+  resolvers,
 });
 
-server.listen(port, "0.0.0.0", () => {
-  console.log("Server running on port", port);
+const yoga = createYoga({
+  schema,
+  graphqlEndpoint: "/graphql",
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://support-ticket-and-sla-tracker-lspf-abee3.vercel.app",
+    ],
+    credentials: true,
+  },
+  context: ({ request }) => {
+    const authHeader = request.headers.get("authorization");
+
+    if (!authHeader) return { user: null };
+
+    const [type, token] = authHeader.split(" ");
+    if (type !== "Bearer" || !token) return { user: null };
+
+    try {
+      const decoded = verifyToken(token);
+      return { user: decoded };
+    } catch {
+      return { user: null };
+    }
+  },
+});
+
+const server = createServer(yoga);
+
+const PORT = Number(process.env.PORT) || 4000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
